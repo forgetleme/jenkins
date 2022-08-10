819158da2a17605e1ddf413323dffb63fbe9a5a2
forgetleme <110952304+forgetleme@users.noreply.github.com>
2022-08-10 13:25:34 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -4,3 +4,17 @@ package org.devops
 def PrintMes(content) {
    println(content)
 }
+
+def PrintMsg(value,color) {
+	//定义字典
+	//value打印信息,color颜色
+    colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+              'blue'  : "\033[47;34m ${value} \033[0m",
+              'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
+              'green1' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
+
+	//调用
+	ansiColor('xterm') {
+		println(colors[color])
+	}
+}
